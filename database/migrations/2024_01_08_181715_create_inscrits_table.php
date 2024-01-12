966f162b497066c8181a604d1f8c27b21bb60204
tiawdev <moussathiaw00@gmail.com>
2024-01-12 15:51:04 +0000

    CSS link restablished, controller and model setted as example

---
+++ database/migrations/2024_01_08_181715_create_inscrits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inscrits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('date_de_naissance');
+            $table->string('lieu_naissance');
+            $table->string('adresse');
+            $table->string('email');
+            $table->string('telephone');
+            $table->string('nationalité');
+            $table->string('niveau');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inscrits');
+    }
+};
